46c1161b30ec9e2707ac34ab535ee6db72e96b19
Arcano <prodasl@hotmail.com>
2025-04-02 14:51:31 -0500

    Update update-node.sh

--- update-node.sh
+++ update-node.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 systemctl stop nym-node
-mkdir bck
+mkdir ~/bck
 backup_name=$(date +%M)
-mkdir bck/$backup_name
-cp nym-node bck/$backup_name/
-wget -q -O nym-node \
-$(wget -q -O - 'https://api.github.com/repos/nymtech/nym/releases/latest' |
+mkdir ~/bck/$backup_name
+cp ~/nym-node ~/bck/$backup_name/
+rm ~/nym-node
+wget -q -O ~/nym-node \
+$(wget -q -O - 'https://api.github.com/repos/nymtech/nym/releases/latest' | \
 jq -r '.assets[] | select(.name=="nym-node").browser_download_url') 
-chmod +x nym-node
+chmod +x ~/nym-node
+cp ~/hashes.json ~/bck/$backup_name/
+rm ~/hashes.json
+wget -q -O ~/hashes.json \
+$(wget -q -O - 'https://api.github.com/repos/nymtech/nym/releases/latest' | \
+jq -r '.assets[] | select(.name=="hashes.json").browser_download_url') 
 systemctl start nym-node
 journalctl -f -u nym-node | grep -i error >> bck/$backup_name/$(date +%M)_error.log &
 journalctl -f -u nym-node | grep -i warn >> bck/$backup_name/$(date +%M)_warn.log &
